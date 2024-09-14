95c15832f00ccf0596531c427918a479fb6813d3
Emmanuel Bobade <emmanuel.bobade@decagon.dev>
2024-09-14 09:38:39 +0100

    Sql modified

--- AlutaMartAPI/SQLQueries/BuyerSQL.cs
+++ AlutaMartAPI/SQLQueries/BuyerSQL.cs
@@ -3,6 +3,6 @@ namespace AlutaMartAPI.SQLQueries;
     public class BuyerSQL
     {
         public static string UpdateAdPurchasedCount => 
-                @"UPDATE ""Buyers"" SET ""AdPurchasedCount"" = ""AdPurchasedCount"" + @quantity WHERE ""Id"" = @buyerId";
+                @"UPDATE ""Buyers"" SET ""AdPurchasedCount"" = ""AdPurchasedCount"" + @quantity, ""Modified"" = now() WHERE ""Id"" = @buyerId";
 
     }
